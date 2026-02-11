6007ca2c33579b0cd4a6d369a15a8878fc8077d1
Ariella Duskis <aduskis@student.touro.edu>
2026-02-11 16:59:31 -0500

    commit message that describes changes

--- src/test/java/org/example/ForceTest.java
+++ src/test/java/org/example/ForceTest.java
@@ -1,4 +1,47 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class ForceTest {
-  
+
+    @Test
+    void testGetX() {
+        Force f = new Force(7.0, 3.0);
+        assertEquals(7.0, f.getX());
+    }
+
+    @Test
+    void testGetY() {
+        Force f = new Force(7.0, 3.0);
+        assertEquals(3.0, f.getY());
+    }
+
+    @Test
+    void testGetMagnitude() {
+        Force f = new Force(7.0, 3.0);
+        assertEquals(7.62, f.getMagnitude(), .01);
+    }
+
+    @Test
+    void testGetRadians() {
+        Force f = new Force(7.0, 3.0);
+        assertEquals(0.40,  f.getRadians(), .01);
+    }
+
+    @Test
+    void testGetDegrees() {
+        Force f = new Force(7.0, 3.0);
+        assertEquals(23.19,  f.getDegrees(), 0.01);
+    }
+
+    /*@Test
+    void testAdd() {
+        Force f = new Force(7.0, 3.0);
+        Force f2 = new Force(-4.0, 12.0);
+
+    }
+
+     */
 }
