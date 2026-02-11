ea94446dd0d70abe866facece02cac86c51f5470
Ariella Duskis <aduskis@student.touro.edu>
2026-02-11 12:27:08 -0500

    initial commit

---
+++ src/main/java/org/example/Force.java
@@ -0,0 +1,39 @@
+package org.example;
+
+public class Force{
+    double x;
+    double y;
+
+    public Force(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX(){
+        return x;
+    }
+
+    public double getY(){
+        return y;
+    }
+
+    public double getMagnitude(){
+        double square1 = x*x;
+        double square2 = y*y;
+        return Math.sqrt(square1 + square2);
+    }
+
+    public double getRadians(){
+        return Math.atan2(y, x);
+    }
+
+    public double getDegrees(){
+        double radian = Math.atan2(y, x);
+        return Math.toDegrees(radian);
+    }
+
+    public Force add(Force f){
+        return new Force(this.x + f.x, this.y + f.y);
+    }
+}
+
