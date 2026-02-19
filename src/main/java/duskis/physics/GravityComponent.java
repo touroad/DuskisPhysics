8b972ae429f001e46f39d0e7a92013f37ba3d90f
Ariella Duskis <aduskis@student.touro.edu>
2026-02-18 19:24:37 -0500

    Finished classwork, ready for homework

--- src/main/java/duskis/physics/GravityComponent.java
+++ src/main/java/duskis/physics/GravityComponent.java
@@ -4,12 +4,18 @@ import javax.swing.*;
 import java.awt.*;
 
 public class GravityComponent extends JComponent {
+    private Force force = new Force(37.065, 28.9360);
+
+    public void setForce(Force force) {
+        this.force = force;
+        repaint();
+    }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponents(g);
 
-        Projectile p = new Projectile(0, 0, new Force(37.065, 28.9360));
+        Projectile p = new Projectile(0, 0, force);
 
         g.translate(0, getHeight());
         for (double i = 0; i < 5; i += 0.001) {
--- src/main/java/duskis/physics/GravityFrame.java
+++ src/main/java/duskis/physics/GravityFrame.java
@@ -2,6 +2,8 @@ package duskis.physics;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class GravityFrame extends JFrame {
 
@@ -12,7 +14,30 @@ public class GravityFrame extends JFrame {
 
         setLayout(new BorderLayout());
 
-        add(new GravityComponent(), BorderLayout.CENTER);
+        GravityComponent gravityComponent = new GravityComponent();
+
+        JTextField xfield = new JTextField("37.065");
+        JTextField yfield = new JTextField("28.9360");
+
+        JButton button = new JButton("Draw");
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                gravityComponent.setForce(
+                        new Force(
+                                Double.parseDouble(xfield.getText()),
+                                Double.parseDouble(yfield.getText())
+                        ));
+            }
+        });
+
+        JPanel northPanel = new JPanel();
+        northPanel.add(xfield);
+        northPanel.add(yfield);
+        northPanel.add(button);
+        add(northPanel, BorderLayout.NORTH);
+
+        add(gravityComponent, BorderLayout.CENTER);
     }
 
 
